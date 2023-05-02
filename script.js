282817fc025fa192aa6a0cb6b7cac29500ca656d
Ro005 <84298912+Roberto-Dantas@users.noreply.github.com>
2023-05-02 18:34:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const q1 = document.querySelector('#quadrante1')
 const q2 = document.querySelector('#quadrante2')
 const q3 = document.querySelector('#quadrante3')
+const q4 = document.querySelector('#quadrante4')
 const fixo = " 2s ease-out forwards"
 var dataObj 
 
@@ -12,16 +13,26 @@ document.querySelectorAll('.conteudo button').forEach(click => {
             q1.style.animation=`go-left${fixo}`
             q2.style.animation=`go-bottom${fixo}`
             q3.style.animation=`go-bottom${fixo}`
+            q4.style.animation=`go-bottom${fixo}`
         }
         else if(click.id === "02"){
             dataObj = {qntQ:10,tempo:3}
             q2.style.animation=`go-left${fixo}`
             q1.style.animation=`go-bottom${fixo}`
             q3.style.animation=`go-bottom${fixo}`
+            q4.style.animation=`go-bottom${fixo}`
         }
         else if(click.id === "03"){
             dataObj = {qntQ:20,tempo:5}
             q3.style.animation=`go-left${fixo}`
+            q4.style.animation=`go-bottom${fixo}`
+            q2.style.animation=`go-bottom${fixo}`
+            q1.style.animation=`go-bottom${fixo}`
+        }
+        else if(click.id === "04"){
+            dataObj = {qntQ:30,tempo:7}
+            q4.style.animation=`go-left${fixo}`
+            q3.style.animation=`go-bottom${fixo}`
             q2.style.animation=`go-bottom${fixo}`
             q1.style.animation=`go-bottom${fixo}`
         }
