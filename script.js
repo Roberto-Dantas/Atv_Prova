e7b9cdd7ab13a3871126526b092032e3d296168d
Ro005 <84298912+Roberto-Dantas@users.noreply.github.com>
2023-09-12 06:19:16 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const botaoClick = document.querySelector('.btn button').addEventListener('click', () => {
+  const username = {['Roberto']}
+  const password = document.getElementByID('password').value
+  if(document.getElementById('username').value === 
+})    
