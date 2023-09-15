7b8c8472ebb66dbd17fe0586864ac83540af0c95
Gut135 <98456118+Gut135@users.noreply.github.com>
2023-09-15 08:49:32 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const botaoClick = document.querySelector('.btn button').addEventListener('click', () => {
-  const username = {['Roberto']}
-  const password = document.getElementByID('password').value
-  if(document.getElementById('username').value === 
-})    
